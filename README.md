2f57716087eee8e91f6247e9279f61590858b4e0
Fernando Correia <fernando@dominodatalab.com>
2017-04-15 13:49:36 -0700

    Test in Docker

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # setup-my-machine
 Automated scripts to set up my machine
+
+## Testing
+
+To test in a Docker container:
+
+```
+$ docker run --rm -it -v $(pwd):/mnt/setup-my-machine ubuntu:16.04 /mnt/setup-my-machine/setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-set -o nounset -o errexit
+set -o nounset -o errexit -o pipefail
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 missing_executable() {
   local executable=$1
   ! which $executable >/dev/null 2>&1
+}
+
+install_prerequisites() {
+  if missing_executable sudo; then
+    echo "Installing sudo"
+    apt update
+    apt install --yes sudo
+  fi
+  if missing_executable add-apt-repository; then
+    echo "Installing add-apt-repository"
+    sudo apt update
+    sudo apt install --yes software-properties-common
+  fi
 }
 
 install_ansible() {
@@ -21,6 +36,8 @@ apply_playbook() {
 }
 
 echo "Setting up machine"
+cd "$SCRIPT_DIR"
+install_prerequisites
 install_ansible
 apply_playbook
 echo "Finished setting up machine"
