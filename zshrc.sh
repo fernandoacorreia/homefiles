66b4ddf25103d40cec9a51f3dac1d75eded5e5bc
Fernando Correia <fernando@dominodatalab.com>
2017-09-12 11:46:49 -0700

    Add completions plugin

--- zshrc.sh
+++ zshrc.sh
@@ -6,6 +6,7 @@ source $HOME/homefiles/vendor/antigen/antigen.zsh
 antigen use oh-my-zsh
 antigen bundle git
 antigen bundle command-not-found
+antigen bundle zsh-users/zsh-completions
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen theme agnoster
 antigen apply
