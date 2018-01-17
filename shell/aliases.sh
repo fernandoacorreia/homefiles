b864200b0ead04483cf60be72e4edec5bbd1cc55
Fernando Correia <fernandoacorreia@gmail.com>
2018-01-17 12:32:57 -0800

    Add helpers to ssh into Domino instances

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -14,10 +14,6 @@ alias sbt-debug="/usr/bin/sbt -mem 4000 -jvm-debug 9997"
 alias domino-nucleus="/usr/bin/sbt -mem 4000 -jvm-debug 9998 -shell 'project nucleus' compile shell"
 alias domino-executor="/usr/bin/sbt -mem 4000 -jvm-debug 9999 -shell 'project executor' compile shell"
 
-sshdom() {
-  ssh -i $HOME/.ssh/domino-test.pem ubuntu@$1
-}
-
 # Kubernetes
 alias kc='kubectl'
 
