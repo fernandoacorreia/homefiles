adb7599eb0d8bc1674e73ce4ba44fff26b912fb4
Fernando Correia <fernandoacorreia@gmail.com>
2018-06-06 14:56:51 -0700

    Correctly source secrets

--- shell/profile.sh
+++ shell/profile.sh
@@ -1,4 +1,4 @@
-source $HOME/secrets/*
+for f in ~/secrets/*.sh; do source $f; done
 source $HOME/homefiles/shell/environment.sh
 source $HOME/homefiles/shell/aliases.sh
 source $HOME/homefiles/shell/less-termcap.sh
