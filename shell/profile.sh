f2c926ae434087ad06f707e8412317e043725e08
Fernando Correia <fernandoacorreia@gmail.com>
2018-09-22 12:14:10 -0700

    Don't fail if there are no secrets

--- shell/profile.sh
+++ shell/profile.sh
@@ -1,4 +1,9 @@
-for f in ~/secrets/*.sh; do source $f; done
+# Import secrets
+if test -n "$(find ~/secrets -maxdepth 1 -name '*.sh' -print -quit)"  # https://stackoverflow.com/a/7702334
+then
+  for f in ~/secrets/*.sh; do source $f; done
+fi
+
 source $HOME/homefiles/shell/environment.sh
 source $HOME/homefiles/shell/aliases.sh
 source $HOME/homefiles/shell/less-termcap.sh
